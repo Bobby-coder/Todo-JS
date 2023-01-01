129c094000e3f974f10f45292cd5686bb81a7eac
Bobby-coder <bobbysadhwani612@gmail.com>
2023-01-01 16:03:40 +0530

    dummy data removed

--- index.js
+++ index.js
@@ -10,8 +10,6 @@ let input = document.querySelector(".input");
 
 let inputHidden = document.querySelector(".input-hidden");
 
-localStorage.setItem('items', JSON.stringify(['Eat, Sleep, Code, Repeat']))
-
 window.addEventListener("load", () => {
   items = JSON.parse(localStorage.getItem("items")) || [];
 
@@ -34,50 +32,49 @@ window.addEventListener("load", () => {
 
     items.forEach((e, i) => {
       let li = document.createElement("li");
-      li.classList.add('single-todo')
+      li.classList.add("single-todo");
       todoList.appendChild(li);
 
-      let span = document.createElement("span")
-      span.textContent=e
-      li.appendChild(span)
+      let span = document.createElement("span");
+      span.textContent = e;
+      li.appendChild(span);
 
       let editBtn = document.createElement("button");
-      editBtn.classList.add("edit-button")
-      editBtn.innerHTML = '<i class="fa-solid fa-pen-to-square"></i>'
+      editBtn.classList.add("edit-button");
+      editBtn.innerHTML = '<i class="fa-solid fa-pen-to-square"></i>';
       li.appendChild(editBtn);
       editBtn.addEventListener("click", () => editItem(i));
 
       let delBtn = document.createElement("button");
-      delBtn.innerHTML='<i class="fa-solid fa-trash"></i>'
+      delBtn.innerHTML = '<i class="fa-solid fa-trash"></i>';
       li.appendChild(delBtn);
       delBtn.addEventListener("click", () => removeItem(i));
     });
 
-    if(items.length > 1){
-      removeAll.style.display='initial'
-    }
-    else{
-      removeAll.style.display='none'
+    if (items.length > 1) {
+      removeAll.style.display = "initial";
+    } else {
+      removeAll.style.display = "none";
     }
   };
 
   showList();
 
   const editItem = (i) => {
-    saveBtn.style.display = 'initial'
-    btn.style.display='none'
-    inputHidden.value=i
-    input.value=items[i]
+    saveBtn.style.display = "initial";
+    btn.style.display = "none";
+    inputHidden.value = i;
+    input.value = items[i];
   };
 
   const saveTodo = () => {
-    saveBtn.style.display='none'
-    btn.style.display='initial'
-    items[inputHidden.value] = input.value
-    input.value=''
-    localStorage.setItem('items', JSON.stringify(items))
-    showList()
-  }
+    saveBtn.style.display = "none";
+    btn.style.display = "initial";
+    items[inputHidden.value] = input.value;
+    input.value = "";
+    localStorage.setItem("items", JSON.stringify(items));
+    showList();
+  };
 
   const removeItem = (i) => {
     items = items.filter((_, ind) => i != ind);
